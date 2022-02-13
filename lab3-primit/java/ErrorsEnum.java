efae0fea49473c7602946bca156624fd1b44b0b0
Random <200087238@aston.ac.uk>
2022-02-13 19:33:05 +0000

    lab3

--- lab3-primit/java/ErrorsEnum.java
+++ lab3-primit/java/ErrorsEnum.java
@@ -3,6 +3,7 @@ import java.util.EnumSet;
 
 public class ErrorsEnum
 {
+
     enum Error { FP_ROUNDING, FP_OVERFLOW, FP_UNDERFLOW, INT_OVERFLOW }
 
     enum Result { A_BIT_DIFFERENT, INFINITY, ZERO, VERY_DIFFERENT }
@@ -54,16 +55,47 @@ public class ErrorsEnum
         return result;
     }
 
+    // 3.2b
+    private static Error result2Error(Result r) {
+        
+        Error error = null;
+        
+        switch (r) {
+        case A_BIT_DIFFERENT:
+            error = Error.FP_ROUNDING;
+            break;
+        case INFINITY:
+            error = Error.FP_OVERFLOW;
+            break;
+        case ZERO:
+            error = Error.FP_UNDERFLOW;
+            break;
+        case VERY_DIFFERENT:
+            error = Error.INT_OVERFLOW;
+            break;
+        }
+
+        return error;
+    }
+
     public static void main(String[] args)
     {
         System.out.print("Known errors = ");
-        for (Error e : EnumSet.allOf(Error.class)) 
+        // for (Error e : EnumSet.allOf(Error.class)) 
+        for (Result r : EnumSet.allOf(Result.class)) 
         {
-            System.out.print(e + " ");
+            System.out.print(r + " ");
         }
         System.out.println();
         
-        Error e = getEnumElement("error", Error.class);
-        System.out.println(e + " results in: " + error2Result(e));
+        // Error e = getEnumElement("error", Error.class);
+        Result r = getEnumElement("result", Result.class);
+        // System.out.println(e + " results in: " + error2Result(e));
+        System.out.println(r + " results in: " + result2Error(r));
     }
 }
+
+/** 
+ * 3.2b
+ * swapping the enum errors and results
+*/
