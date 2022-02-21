ab6f71df9ab7cd8b0e1d4e0ecb6b39e6880ff5ca
Random <200087238@aston.ac.uk>
2022-02-21 23:37:10 +0000

    lab4

--- lab4-records/haskell/playlist.hs
+++ lab4-records/haskell/playlist.hs
@@ -43,26 +43,26 @@ piece2 =
         item_performer = Person "Daniel Barenboim",
         item_length_secs = 16*60+49
     }
-  
-{-
+
+{
 pause1 =
     Pause
-    { 
+    {
         item_length_secs = 5
     }
--}
+}
 
 main =
     do
-    -- putStrLn "piece1 and piece2 sorted by length:"
-    -- putStrLn $ show shorterPiece
-    -- putStrLn $ show longerPiece
+    putStrLn "piece1 and piece2 sorted by length:"
+    putStrLn $ show shorterPiece
+    putStrLn $ show longerPiece
     putStr "piece1 = "
     putStrLn $ show piece1
---    putStr "pause1 = "
---    putStrLn $ show pause1
+    putStr "pause1 = "
+    putStrLn $ show pause1
 
--- ... = sortTwoItems (piece1, piece2) -- TASK
+(shorterPiece, longerPiece) = sortTwoItems (piece1, piece2) -- TASK
 
 sortTwoItems (item1, item2) = 
     if item_length_secs item1 <= item_length_secs item2
